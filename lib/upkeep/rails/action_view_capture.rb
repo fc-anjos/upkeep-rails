a817a8c8ce98dbbb9a6f57aa74017cd58ad3e059
fc-anjos <felipe.cavalheiro.anjos@gmail.com>
2026-05-11 20:09:44 +0100

    Add Rails ActionView replay recipes

--- lib/upkeep/rails/action_view_capture.rb
+++ lib/upkeep/rails/action_view_capture.rb
@@ -22,15 +22,39 @@ module Upkeep
         !!@installed
       end
 
-      def capture_template(template, locals)
-        metadata = template_metadata(template, locals)
-        Runtime::Observation.capture_frame(frame_id_for_template(metadata, locals), metadata) { yield }
+      def capture_template(template, view, locals, implicit_locals:, add_to_stack:, block:)
+        captured_locals = locals.dup
+        metadata = template_metadata(template, captured_locals)
+        frame_id = frame_id_for_template(metadata, captured_locals)
+        recipe = template_recipe(
+          frame_id: frame_id,
+          template: template,
+          view: view,
+          locals: captured_locals,
+          metadata: metadata,
+          implicit_locals: implicit_locals,
+          add_to_stack: add_to_stack,
+          block: block
+        )
+
+        Runtime::Observation.capture_frame(frame_id, metadata.merge(recipe: recipe)) { yield }
       end
 
-      def capture_collection(partial, collection)
+      def capture_collection(partial, collection, context, options, block)
+        captured_options = render_options_for_replay(options)
         metadata = collection_metadata(partial, collection)
+        frame_id = "site:#{metadata.fetch(:site_id)}"
+        recipe = collection_recipe(
+          frame_id: frame_id,
+          partial: partial,
+          collection: collection,
+          context: context,
+          options: captured_options,
+          metadata: metadata,
+          block: block
+        )
 
-        Runtime::Observation.capture_frame("site:#{metadata.fetch(:site_id)}", metadata) do
+        Runtime::Observation.capture_frame(frame_id, metadata.merge(recipe: recipe)) do
           record_collection_dependency(collection)
           yield
         end
@@ -56,6 +80,42 @@ module Upkeep
           partial: partial.to_s,
           collection: collection_key
         }
+      end
+
+      def template_recipe(frame_id:, template:, view:, locals:, metadata:, implicit_locals:, add_to_stack:, block:)
+        target_kind = metadata.fetch(:kind) == "fragment" ? "fragment" : "page"
+        Replay::Recipe.new(
+          kind: metadata.fetch(:kind).to_sym,
+          frame_id: frame_id,
+          target_kind: target_kind,
+          target_id: frame_id,
+          template: metadata.fetch(:template),
+          metadata: metadata
+        ) do
+          template.render(
+            view,
+            replay_locals(locals),
+            nil,
+            implicit_locals: implicit_locals,
+            add_to_stack: add_to_stack,
+            &block
+          )
+        end
+      end
+
+      def collection_recipe(frame_id:, partial:, collection:, context:, options:, metadata:, block:)
+        Replay::Recipe.new(
+          kind: :render_site,
+          frame_id: frame_id,
+          target_kind: "render_site",
+          target_id: metadata.fetch(:site_id),
+          metadata: metadata
+        ) do
+          replay_options = replay_render_options(options)
+          replay_options[:partial] = partial unless partial == :derived
+          replay_options[:collection] = replay_value(collection)
+          context.render(replay_options, &block)
+        end
       end
 
       def record_collection_dependency(collection)
@@ -101,6 +161,34 @@ module Upkeep
         end
       end
 
+      def render_options_for_replay(options)
+        options.each_with_object({}) do |(key, value), replay_options|
+          replay_options[key] = key == :locals && value.respond_to?(:dup) ? value.dup : value
+        end
+      end
+
+      def replay_render_options(options)
+        options.each_with_object({}) do |(key, value), replay_options|
+          replay_options[key] = key == :locals ? replay_locals(value || {}) : value
+        end
+      end
+
+      def replay_locals(locals)
+        locals.transform_values { |value| replay_value(value) }
+      end
+
+      def replay_value(value)
+        if value.is_a?(ActiveRecord::Base)
+          value.class.find(value.id)
+        elsif value.respond_to?(:spawn) && value.respond_to?(:klass)
+          value.spawn
+        elsif value.is_a?(Array)
+          value.map { |item| replay_value(item) }
+        else
+          value
+        end
+      end
+
       def collection_key(collection)
         if collection.respond_to?(:klass) && collection.respond_to?(:to_sql)
           {
@@ -120,7 +208,14 @@ module Upkeep
 
       module TemplateHook
         def render(view, locals, buffer = nil, implicit_locals: [], add_to_stack: true, &block)
-          Upkeep::Rails::ActionViewCapture.capture_template(self, locals) do
+          Upkeep::Rails::ActionViewCapture.capture_template(
+            self,
+            view,
+            locals,
+            implicit_locals: implicit_locals,
+            add_to_stack: add_to_stack,
+            block: block
+          ) do
             super
           end
         end
@@ -128,13 +223,13 @@ module Upkeep
 
       module CollectionRendererHook
         def render_collection_with_partial(collection, partial, context, block)
-          Upkeep::Rails::ActionViewCapture.capture_collection(partial, collection) do
+          Upkeep::Rails::ActionViewCapture.capture_collection(partial, collection, context, @options, block) do
             super
           end
         end
 
         def render_collection_derive_partial(collection, context, block)
-          Upkeep::Rails::ActionViewCapture.capture_collection(:derived, collection) do
+          Upkeep::Rails::ActionViewCapture.capture_collection(:derived, collection, context, @options, block) do
             super
           end
         end
