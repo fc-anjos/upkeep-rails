a4aef5e667e81f2c8f1a8a3bfe82fc02c4b2cf3f
fc-anjos <felipe.cavalheiro.anjos@gmail.com>
2026-05-13 22:06:48 +0100

    Report Herb coverage guidance

--- test/benchmark/layout_test.rb
+++ test/benchmark/layout_test.rb
@@ -62,6 +62,7 @@ class BenchmarkLayoutTest < Minitest::Test
     refute_includes files, "lib/upkeep/proof_support.rb"
     refute_includes files, "lib/upkeep/proofs/end_to_end.rb"
     refute_includes files, "lib/upkeep/probes/herb_surface.rb"
+    refute_includes files, "lib/upkeep/herb/developer_report.rb"
     refute_includes files, "lib/upkeep/herb/fallback_analyzer.rb"
     refute_includes files, "lib/upkeep/herb/manifest_diff.rb"
     refute_includes files, "lib/upkeep/herb/runtime_alignment.rb"
