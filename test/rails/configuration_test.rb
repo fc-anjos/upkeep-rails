408cb8f437636ea088af8ed5e5ccc032ad98afc9
fc-anjos <felipe.cavalheiro.anjos@gmail.com>
2026-08-02 13:46:12 -0300

    Add opt-in reactive request activation

--- test/rails/configuration_test.rb
+++ test/rails/configuration_test.rb
@@ -13,4 +13,26 @@ class ConfigurationTest < Minitest::Test
 
     assert_equal 60 * 60, configuration.subscription_ttl
   end
+
+  def test_request_activation_defaults_to_all
+    assert_equal :all, Upkeep::Rails::Configuration.new.request_activation
+  end
+
+  def test_request_activation_accepts_opt_in
+    configuration = Upkeep::Rails::Configuration.new
+
+    configuration.request_activation = :opt_in
+
+    assert_equal :opt_in, configuration.request_activation
+  end
+
+  def test_request_activation_rejects_unknown_values
+    configuration = Upkeep::Rails::Configuration.new
+
+    error = assert_raises(Upkeep::Rails::ConfigurationError) do
+      configuration.request_activation = :sometimes
+    end
+
+    assert_match(/expected one of all, opt_in/, error.message)
+  end
 end
